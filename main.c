cc24fe07fb33c789fab1589e4d217b7a6a4a529e
AlexrAlves <alex2cra7@hotmail.com>
2023-01-20 19:00:27 -0300

    fix pointer

--- main.c
+++ main.c
@@ -11,24 +11,24 @@ int main()
     printf("Counter IP Test!\n");
 
     uint32_t count_in = 16;
-    uint32_t init_value, count_val, count_val2;
+    uint32_t * init_value, count_val, count_val2;
     
     get_count(init_value);
 
-    printf("Initial Value: %d\n", init_value);
+    printf("Initial Value: %d\n", *init_value);
 
     enable_counter();
     get_count(count_val);
     get_count(count_val2);
     disable_counter();
 
-    printf("First reading: count = %d\n", count_val);
-    printf("Second reading: count = %d\n", count_val2);
+    printf("First reading: count = %d\n", *count_val);
+    printf("Second reading: count = %d\n", *count_val2);
 
     get_count(count_val);
 
-    printf("After disabling: count = %d\n", count_val);
-    printf("After disabling: count = %d\n", count_val);
+    printf("After disabling: count = %d\n", *count_val);
+    printf("After disabling: count = %d\n", *count_val);
 
     return 0;
 }
